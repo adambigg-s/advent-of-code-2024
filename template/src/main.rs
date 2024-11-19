01bc3bdd8f26d8db00edd324505837028fae8ea7
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2024-11-19 17:28:54 -0500

    fix

---
+++ template/src/main.rs
@@ -0,0 +1,30 @@
+
+
+
+use std::{env, fs, process};
+
+
+
+fn main() {
+    let envs: Vec<String> = env::args().collect();
+    let mut testing: bool = false;
+    let file_path: &str = envs.get(1).map(|path| path.as_str()).unwrap_or_else(|| {
+        testing = true;
+        "testing.txt"
+    });
+    let buffer: String = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        println!("buffer read error: {err}");
+        process::exit(3);
+    });
+    if testing { println!("{buffer}"); }
+
+    Solution::solve();
+}
+
+struct Solution;
+
+impl Solution {
+    fn solve() -> () {
+        
+    }
+}
