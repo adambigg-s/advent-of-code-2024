0208b409b88c4b381a075c3c6928aa105b4d8a8e
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2024-12-01 00:48:19 -0500

    day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,7 +1,7 @@
 
 
 
-use std::{env, fs, process};
+use std::{env, fs, process, collections::HashMap};
 
 
 
@@ -18,13 +18,59 @@ fn main() {
     });
     if testing { println!("{buffer}"); }
 
-    Solution::solve();
+    let part_1 = Solution::solve_one(&buffer);
+    println!("part 1: {part_1}");
+    let part_2 = Solution::solve_two(&buffer);
+    println!("part 2: {part_2}");
 }
 
 struct Solution;
 
 impl Solution {
-    fn solve() -> () {
-        
+    fn solve_one(buffer: &str) -> i32 {
+        let mut list1: Vec<i32> = Vec::new();
+        let mut list2: Vec<i32> = Vec::new();
+        let mut diff: i32 = 0;
+
+        for line in buffer.lines() {
+            let mut nums = line.split_whitespace();
+            let first = nums.nth(0).unwrap();
+            let last = nums.nth(0).unwrap();
+            list1.push(first.parse::<i32>().unwrap());
+            list2.push(last.parse::<i32>().unwrap());
+        }
+
+        list1.sort_unstable();
+        list2.sort_unstable();
+
+        {
+            debug_assert!(list1.len() == list2.len());
+        }
+        for idx in 0..list1.len() {
+            diff += (list1[idx] - list2[idx]).abs();
+        }
+
+        diff
+    }
+
+    fn solve_two(buffer: &str) -> i32 {
+        let mut list: Vec<i32> = Vec::new();
+        let mut hashmap: HashMap<i32, i32> = HashMap::new();
+        let mut diff: i32 = 0;
+
+        for line in buffer.lines() {
+            let mut nums = line.split_whitespace();
+            let first = nums.nth(0).unwrap();
+            let last = nums.nth(0).unwrap();
+            list.push(first.parse::<i32>().unwrap());
+            *hashmap.entry(last.parse::<i32>().unwrap()).or_insert(0) += 1;
+        }
+
+        for num in &list {
+            let freq = hashmap.get(num).unwrap_or(&0);
+            diff += freq * num;
+        }
+
+        diff
     }
 }
