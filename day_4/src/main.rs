8267211a2d07b1dc4f6922ea9b651d9854ce3f6f
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2024-12-04 00:54:30 -0500

    day 4

---
+++ day_4/src/main.rs
@@ -0,0 +1,138 @@
+
+
+
+use std::{env, fs, process};
+
+
+
+fn main() {
+    let envs: Vec<String> = env::args().collect();
+    let mut testing: bool = false;
+    let file_path: &str = envs.get(1).map(|path| path.as_str()).unwrap_or_else(|| {
+        testing = true;
+        "testing.txt"
+    });
+    let buffer: String = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        println!("buffer read error: {err}");
+        process::exit(3);
+    });
+    if testing {
+        println!("{buffer}");
+    }
+
+    let solution: Solution = Solution::construct(&buffer);
+    let instances_one: i32 = solution.solve_one();
+    println!("part one: {instances_one}");
+    let instances_two: i32 = solution.solve_two();
+    println!("part two: {instances_two}");
+}
+
+struct Solution {
+    grid: Vec<Vec<char>>,
+    m: usize, 
+    n: usize,
+}
+
+impl Solution {
+    fn construct(buffer: &str) -> Solution {
+        let grid = Self::gridify(buffer);
+        let m = grid.len();
+        let n = grid.first().unwrap().len();
+
+        Solution { grid, m, n }
+    }
+
+    fn solve_one(&self) -> i32 {
+        let mut occurances: i32 = 0;
+        for i in 0..self.m {
+            for j in 0..self.n {
+                occurances += self.search(i, j);
+            }
+        }
+
+        occurances
+    }
+
+    fn solve_two(&self) -> i32 {
+        let mut occurances: i32 = 0;
+        for i in 0..self.m {
+            for j in 0..self.n {
+                let found = self.cross_search(i, j);
+                occurances += found / 2;
+            }
+        }
+
+        occurances
+    }
+
+    fn cross_search(&self, i: usize, j: usize) -> i32 {
+        let mut occurances: i32 = 0;
+        let directions: Vec<(isize, isize)> = vec![
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+
+        if self.grid[i][j] != 'A' {
+            return 0;
+        }
+
+        for (dx, dy) in directions {
+            let (nx, ny) = Self::idx(i, j, dx, dy);
+            let (nxp, nyp) = Self::idx(i, j, -dx, -dy);
+            if self.inbounds(nx, ny) && self.grid[ny][nx] == 'M' && self.inbounds(nxp, nyp) && self.grid[nyp][nxp] == 'S' {
+                occurances += 1;
+            }
+        }
+        
+        occurances
+    }
+
+    fn search(&self, i: usize, j: usize) -> i32 {
+        let mut occurances: i32 = 0;
+        let directions: Vec<(isize, isize)> = vec![
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+        let target: Vec<char> = vec!['X', 'M', 'A', 'S'];
+
+        for (dx, dy) in directions {
+            if self.grid[i][j] == target[0] {
+                let (nx, ny) = Self::idx(i, j, dx, dy);
+                if self.inbounds(nx, ny) && self.grid[ny][nx] == target[1] {
+                    let (nx, ny) = Self::idx(ny, nx, dx, dy);
+                    if self.inbounds(nx, ny) && self.grid[ny][nx] == target[2] {
+                        let (nx, ny) = Self::idx(ny, nx, dx, dy);
+                        if self.inbounds(nx, ny) && self.grid[ny][nx] == target[3] {
+                            occurances += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        occurances
+    }
+
+    fn inbounds(&self, x: usize, y: usize) -> bool {
+        x < self.n && y < self.m
+    }
+
+    fn idx(y: usize, x: usize, dx: isize, dy: isize) -> (usize, usize) {
+        let nx = (x as isize + dx) as usize;
+        let ny = (y as isize + dy) as usize;
+
+        (nx, ny)
+    }
+
+    fn gridify(buffer: &str) -> Vec<Vec<char>> {
+        buffer.lines().map(|line| line.chars().collect()).collect()
+    }
+}
