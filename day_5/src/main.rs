7ef39c316a7e55c28e857ccc820716352995a441
abiggs33 <146982420+abiggs33@users.noreply.github.com>
2024-12-04 19:11:17 -0500

    fixed template

---
+++ day_5/src/main.rs
@@ -0,0 +1,51 @@
+
+
+
+use std::{env, fs, process, time::Instant};
+
+
+
+fn main() {
+    let envs: Vec<String> = env::args().collect();
+    let mut testing: bool = false;
+    let file_path: &str = envs.get(1).map(|path| path.as_str()).unwrap_or_else(|| {
+        testing = true;
+        "testing.txt"
+    });
+    let buffer: String = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        println!("buffer read error: {err}");
+        process::exit(3);
+    });
+    if testing { println!("{buffer}"); }
+
+    let solution: Solution = Solution::construct();
+
+    let s1 = Instant::now();
+    let part_one = solution.solve_one();
+    let p1 = s1.elapsed();
+
+    let s2 = Instant::now();
+    let part_two = solution.solve_two();
+    let p2 = s2.elapsed();
+
+    println!("part one: {}\ntime one: {:#?}", part_one, p1);
+    println!();
+    println!("part two: {}\ntime two: {:#?}", part_two, p2);
+    println!();
+}
+
+struct Solution;
+
+impl Solution {
+    fn construct() -> Solution {
+        Solution
+    }
+
+    fn solve_one(&self) -> i32 {
+        0
+    }
+
+    fn solve_two(&self) -> i32 {
+        0
+    }
+}
